10f5e79b01d01862a034f3706dcbd8d9fca47297
Yohanan Assefa <nanahoy@gmail.com>
2017-03-19 13:25:26 -0600

    Add while and until loops

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,4 @@
+require "pry"
 
 class BubbleSort
 
@@ -25,3 +26,51 @@ end
 sorter = BubbleSort.new
 sorter.sort(["d", "b", "a", "c"])
 sorter.sort([5, 4, 0, 1, 2, 3])
+
+------------------------------------------------------------------------
+
+class BubbleSorter
+
+  def sort(unsorted)
+      sorted = false
+      until sorted
+        sorted = true
+          (unsorted.count - 1).times do |index|
+            if unsorted[index] > unsorted[index + 1]
+              unsorted[index], unsorted[index + 1] = unsorted[index + 1], unsorted[index]
+              sorted = false
+            end
+          end
+      end
+    unsorted
+  end
+
+end
+
+sorter = BubbleSorter.new
+print sorter.sort(["d", "b", "a", "c"])
+print sorter.sort([5, 4, 0, 1, 2, 3])
+#
+----------------------------------------------------------------------------
+
+class BubbleSorting
+
+  def sort(unsorted)
+    unarranged = true
+      while unarranged
+        unarranged = false
+        (unsorted.count - 1).times do |index|
+          if unsorted[index] > unsorted[index + 1]
+            unsorted[index], unsorted[index + 1] = unsorted[index + 1], unsorted[index]
+            unarranged = true
+          end
+        end
+      end
+    unsorted
+  end
+end
+
+
+sorter = BubbleSorting.new
+print sorter.sort(["d", "b", "a", "c"])
+print sorter.sort([5, 4, 0, 1, 2, 3])
