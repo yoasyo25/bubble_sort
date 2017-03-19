1ea1935c63669c8f70a8eb595ac92d6c86ddcb12
Yohanan Assefa <nanahoy@gmail.com>
2017-03-19 10:37:22 -0600

    Bubble sort using loop do

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,1 +1,27 @@
-0 2 1 3 4 5 
+
+class BubbleSort
+
+  def sort(unsorted)
+    n = unsorted.length
+
+    loop do
+
+      sort = false
+
+      (n-1).times do |index|
+        if unsorted[index+1] < unsorted[index]
+          unsorted[index+1], unsorted[index] = unsorted[index], unsorted[index+1]
+          sort = true
+        end
+      end
+
+      break if not sort
+    end
+
+    print unsorted
+  end
+end
+
+sorter = BubbleSort.new
+sorter.sort(["d", "b", "a", "c"])
+sorter.sort([5, 4, 0, 1, 2, 3])
